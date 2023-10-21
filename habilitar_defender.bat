84e53f816bff6d3107e13edc82d174b34fe336b9
オタコン悪 <otaconeviloficial@gmail.com>
2023-10-21 13:20:17 -0300

    Update habilitar_defender.bat

--- habilitar_defender.bat
+++ habilitar_defender.bat
@@ -1,7 +1,7 @@
 @echo off
 chcp 65001 > nul
 color 30
-title https://github.com/OtaconEvil
+title https://github.com/StarkTechRepo
 
 echo Habilitando Windows Defender...
 powershell.exe -Command Set-MpPreference -DisableRealtimeMonitoring 0
